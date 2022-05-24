de0bc1688d69cfb4eb626049cdf8bc016a29b28d
Sami <bsef19m045@pucit.edu.pk>
2022-05-24 06:43:45 +0500

    XMLs Done

---
+++ app/src/main/java/com/example/myapplication/QiuzActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QiuzActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.qiuz);
+    }
+}
